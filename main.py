6ca4bf1cc683295fae581164f67e3c73b205a641
Daniel Jimenez <djwhtone@yahoo.com>
2023-10-27 00:35:41 -0400

    Testing Group function

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ def showAllRiders():
 # Initial menu to guide user
 def menu():
     print("Start of the program")
+    print("try this")
 
     print("add - Enter rider info")
     print("show - Show riders")
