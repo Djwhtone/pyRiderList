1735f612780fff89758f9f90d371d93ac5f3edef
Djwhtone <81643057+Djwhtone@users.noreply.github.com>
2024-09-03 18:32:32 -0400

    Reload class Riders

---
+++ nameObj.py
@@ -0,0 +1,9 @@
+# Rider class stores variables
+class Riders:
+    def __init__(self, id=0, name=None, shift=None,location=None, group_label=None):
+        self.id = id
+        self.name = name
+        self.shift = shift
+        self.location = location
+        self.group_label = group_label
+#push test
